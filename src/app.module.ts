ef075ffadb40f024af0c7c7e181ff1c3e9f2abde
silrith <berkozerdogan@gmail.com>
2025-12-02 15:30:29 +0300

    Transaction Module created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TransactionModule } from './transaction/transaction.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: () => {
+        const uri = process.env.ICEBERGDB;
+        if (!uri) {
+          throw new Error('ICEBERGDB is not defined in .env');
+        }
+        return {
+          uri,
+          dbName: 'Iceberg',
+        };
+      },
+    }),
+    TransactionModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
